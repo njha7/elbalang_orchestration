38511947fccf974ac0aad031e0cf7e38ffc57d8f
dev <you@example.com>
2018-03-05 00:29:47 -0500

    Dockerized Flask applet

--- infrastructure/flask/src/app.py
+++ infrastructure/flask/src/app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def processLog():
   #TODO handle uploaded file with elbalang
   if request.method == 'POST':
-    # return 'luhmoa'
     return request.data
 
 # def auth():
   #TODO provide a token for users to use that allows access to the API
+
+if __name__ == "__main__":
+  app.run(debug = True, host = '0.0.0.0')
