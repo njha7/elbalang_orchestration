d731ed1b559851d728cc6c4ae5919d483ee9d74a
dev <you@example.com>
2018-04-15 17:53:44 -0400

    Cleaning the directories out.

--- infrastructure/flask/src/test.py
+++
@@ -1,3 +0,0 @@
-import analyze_dataset
-
-print(analyze_dataset.analyze_one_file('sar-CA-CJDBC2-20140414205404.data'))
--- scripts/r&d/testSend.py
+++
@@ -1,8 +0,0 @@
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-for x in range(1,100):
-  channel.basic_publish(exchange='', routing_key='hello', body='%d' % x)
-connection.close()
